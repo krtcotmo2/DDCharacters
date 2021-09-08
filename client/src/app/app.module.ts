3fa9416573dafb56cb6ba14a71de3fe09bd3a329
krtcotmo2 <krtcotmo2@gmail.com>
2021-09-08 11:51:56 -0400

    continue tweak

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,18 +16,18 @@ import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
-// const config: SocketIoConfig = {
-//   url: 'http://http://dd-characters.herokuapp.com',
-//   options: {}
-// };
-const config = SocketIoModule.forRoot({
-  url: `wss://${document.location.host}`,
-  options: {
-    transports: ['websocket'],
-    upgrade : true,
-    path: 'http://localhost:4200/socket.io'
-  }
-});
+const config: SocketIoConfig = {
+  url: 'http://dd-characters.herokuapp.com',
+  options: {}
+};
+// const config = SocketIoModule.forRoot({
+//   url: `wss://${document.location.host}`,
+//   options: {
+//     transports: ['websocket'],
+//     upgrade : true,
+//     path: 'http://localhost:4200/socket.io'
+//   }
+// });
 
 @NgModule({
   declarations: [
