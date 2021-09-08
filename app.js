fea20a0c3042a29510b2834af255e91fbbd7567c
krtcotmo2 <krtcotmo2@gmail.com>
2021-09-08 09:45:22 -0400

    still tweaking

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const cors = require('cors');
 
 const io = require('socket.io')(server, {
     cors: {
-      origins: 'http://localhost:4200',
+      origins: 'http://dd-characters.herokuapp.com',
       credentials: true,
       methods: ["GET", "POST"],
     }
