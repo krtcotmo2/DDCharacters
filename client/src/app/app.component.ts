e81fd6e993bce7303be5de8796e8af460c9da503
Kurt Cooney <KCooney@interdev.com>
2024-03-09 18:43:27 -0500

    added error catch

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -32,7 +32,15 @@ export class AppComponent implements OnInit, OnDestroy{
         this.userName = this.theUser['userName'];
       });
       this.userService.checkLoggedInStatus({}).pipe(
-        catchError(err => of({}))
+        catchError(err => of({
+          "userID": 0,
+          "userName": "",
+          "userEmail": "",
+          "forcedReset": false,
+          "createdAt": "",
+          "updatedAt": "2022-08-28 02:19:40",
+          "authorized": false
+      }))
       ).subscribe((val)=>{
         this.theUser = val;
         this.isLoggedIn = true;
